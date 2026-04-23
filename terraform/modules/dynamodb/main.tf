599fca3786789c0079ca4589d950b6356bdce240
Heath Smith <heathsmithaws2025@gmail.com>
2026-04-23 13:42:31 -0400

    Testing tfec via pipeline

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -1,3 +1,12 @@
+resource "aws_kms_key" "dynamodb" {
+  description             = "KMS key for DynamoDB encryption"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+}
+resource "aws_kms_alias" "dynamodb" {
+  name          = "alias/dynamodb-app"
+  target_key_id = aws_kms_key.dynamodb.key_id
+}
 resource "aws_dynamodb_table" "app_table" {
   name         = "ai-serverless-app-table"
   billing_mode = "PAY_PER_REQUEST"
@@ -10,6 +19,7 @@ resource "aws_dynamodb_table" "app_table" {
 
   server_side_encryption {
   enabled = true
+  kms_key_arn = aws_kms_key.dynamodb.arn
 }
 
   tags = {
